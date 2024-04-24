12109f8f0165abd80cc7086e016f8989fd854923
Victor Torres <victor.torrest@ciencias.unam.mx>
2024-04-24 00:51:54 -0600

    Login

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, redirect, render_template, url_for, request, flash, session, jsonify
 
 from controller.catalogue import catalogue
-
+from classes.Usuario import obtenerUsuario
 app = Flask(__name__)
 app.register_blueprint(catalogue)
 app.config['SECRET_KEY'] = 'dev'
@@ -12,6 +12,7 @@ def hello_world():
 
 @app.route('/api/login', methods=['GET','POST'])
 def login():
+<<<<<<< HEAD
     ##Si ya hay un usuario en la cookie, indicar que esta logeado
     if session.get('user_id') != None:
         return jsonify({
@@ -38,12 +39,27 @@ def login():
     else:
         return jsonify({"logged" : False,
                 "user" : None})
+=======
+    if request.method == 'GET':
+        return jsonify({'error': 'Nombre de usuario o contraseña no ingresados'}), 401
+    idUsuario = request.form.get('username')
+    passwd = request.form.get('password')
+    if authenticate(idUsuario, passwd): #Ustedes van a tener que cambiar esto, por una validación con la DB.
+        usuarioDB = obtenerUsuario(idUsuario)
+        session['user_id'] = usuarioDB.id #definición de cookie de sesión.
+        return {'id': usuarioDB.id, 'username': usuarioDB.username}
+    return jsonify({'error': 'Nombre de usuario o contraseña incorrectos'}), 401
+>>>>>>> bcfbfab (Login)
 
 
 @app.route('/api/logout')
 def logout():
     session['user_id'] = None
+<<<<<<< HEAD
     return redirect(url_for('/api/login'))
+=======
+    return jsonify({'message': 'Saliste de la sesion'}), 200
+>>>>>>> bcfbfab (Login)
 
 
 if __name__ == '__main__':
