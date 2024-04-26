0967ef1a88410057f384a6d6e3a5048777ea0bec
RicardoSanPer <ricardosp@ciencias.unam.mx>
2024-04-26 01:29:18 -0600

    Cambiada insercion de comprador a vendedor

--- insert_user.sql
+++ insert_user.sql
@@ -1,2 +1,2 @@
-INSERT INTO comprador (numero_cuenta, nombres, ap_pat, ap_mat, num_telefono, correo, genero, password)
-VALUES (12345, "Fulano", "Zutano", "Carrasco", "123456789", "email@domain.com", "Masculino", "12345");
+INSERT INTO vendedor (numero_cuenta, nombres, ap_pat, ap_mat, num_telefono, correo, genero, password)
+VALUES ("12345", "Fulano", "Zutano", "Carrasco", "123456789", "email@domain.com", "Masculino", "12345");
