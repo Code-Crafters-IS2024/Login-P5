aa3b0ae9b7368d683093ee578099db6c9e387e3c
Victor Torres <victor.torrest@ciencias.unam.mx>
2024-04-26 00:37:03 -0600

    Auth

--- authenticate.py
+++ authenticate.py
@@ -5,7 +5,7 @@ import bcrypt
 
 def authenticate_user(username, password):
     try:
-        user = User.query.filter_by(username=username).one()
+        user = obtener_vendedor(username)
         if bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
             return True
         else:
