13ece09f7ff73edc5579e2d65c481e6f25a7742f
Victor Torres <victor.torrest@ciencias.unam.mx>
2024-04-25 22:50:28 -0600

    Login logout

--- app.py
+++ app.py
@@ -13,24 +13,19 @@ def hello_world():
 
 @app.route('/api/login', methods=['GET','POST'])
 def login():
-<<<<<<< HEAD
-    ##Si ya hay un usuario en la cookie, indicar que esta logeado
     if session.get('user_id') != None:
         return jsonify({
             "logged" : True,
             "user" : session['user_id']
             })
-    
-    ##Si el metodo es get, probablemente es la primera vez que el usuario entra a la pagina
-    ## y devolver false
     if request.method == 'GET':
         return jsonify({"logged" : False,
                 "user" : None})
 
     name = request.form.get('username')
     passwd = request.form.get('password')
-    ## Si se esta intentando logear a travez de un formulario, validar y devolver el resultado
-    if name == '123' and passwd == '123': ##Aqui se puede reemplazar con una consula a la bdS
+    if authenticate_user(name, passwd):
+
         session['user_id'] = name #definición de cookie de sesión.
         return jsonify({
             "logged" : True,
@@ -40,25 +35,12 @@ def login():
     else:
         return jsonify({"logged" : False,
                 "user" : None})
-=======
-    if request.method == 'GET':
-        return jsonify({'error': 'Nombre de usuario o contraseña no ingresados'}), 401
-    idUsuario = request.form.get('username')
-    passwd = request.form.get('password')
-    if authenticate_user(idUsuario, passwd):
-        usuarioDB = obtenerUsuario(idUsuario)
-        session['user_id'] = usuarioDB.id 
-        return {'id': usuarioDB.id, 'username': usuarioDB.username}
-    return jsonify({'error': 'Nombre de usuario o contraseña incorrectos'}), 401
->>>>>>> bcfbfab (Login)
 
+    
 
 @app.route('/api/logout')
 def logout():
     session['user_id'] = None
-<<<<<<< HEAD
-    return redirect(url_for('/api/login'))
-=======
     return jsonify({'message': 'Saliste de la sesion'}), 200
 >>>>>>> bcfbfab (Login)
 
