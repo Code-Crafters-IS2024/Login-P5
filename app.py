f21f579d265c5a6c3ba72c260929d323288870bd
RicardoSanPer <ricardosp@ciencias.unam.mx>
2024-04-25 20:37:31 -0600

    backend

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, url_for, request, flash, session
+from flask import Flask, redirect, render_template, url_for, request, flash, session, jsonify
 
 from controller.catalogue import catalogue
 
@@ -6,31 +6,44 @@ app = Flask(__name__)
 app.register_blueprint(catalogue)
 app.config['SECRET_KEY'] = 'dev'
 
-
 @app.route('/')
 def hello_world():
-    return redirect(url_for('login'))
+    return redirect(url_for('/api/login'))
 
-
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/api/login', methods=['GET','POST'])
 def login():
+    ##Si ya hay un usuario en la cookie, indicar que esta logeado
     if session.get('user_id') != None:
-        return render_template('login.html', user=session['user_id'])
+        return jsonify({
+            "logged" : True,
+            "user" : session['user_id']
+            })
+    
+    ##Si el metodo es get, probablemente es la primera vez que el usuario entra a la pagina
+    ## y devolver false
     if request.method == 'GET':
-        return render_template('index.html')
+        return jsonify({"logged" : False,
+                "user" : None})
+
     name = request.form.get('username')
     passwd = request.form.get('password')
-    if name == 'ferfong' and passwd == 'Developer123!': #Ustedes van a tener que cambiar esto, por una validación con la DB.
+    ## Si se esta intentando logear a travez de un formulario, validar y devolver el resultado
+    if name == '123' and passwd == '123': ##Aqui se puede reemplazar con una consula a la bdS
         session['user_id'] = name #definición de cookie de sesión.
-        return render_template('login.html', user=name)
-    flash('Invalid username or password')
-    return redirect(url_for('login'))
+        return jsonify({
+            "logged" : True,
+            "user" : session['user_id']
+            })
+
+    else:
+        return jsonify({"logged" : False,
+                "user" : None})
 
 
-@app.route('/logout')
+@app.route('/api/logout')
 def logout():
     session['user_id'] = None
-    return redirect(url_for('login'))
+    return redirect(url_for('/api/login'))
 
 
 if __name__ == '__main__':
